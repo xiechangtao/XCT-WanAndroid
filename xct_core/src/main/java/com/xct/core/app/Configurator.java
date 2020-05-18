20ad8e4c5adddffe4aec30d9f6a9d5db7cf8bf55
xiechangtao <xiechangtao@skyworth.com>
2020-05-18 17:54:02 +0800

    second commit

--- xct_core/src/main/java/com/xct/core/app/Configurator.java
+++ xct_core/src/main/java/com/xct/core/app/Configurator.java
@@ -1,5 +1,19 @@
 package com.xct.core.app;
 
+import android.os.Handler;
+
+import com.blankj.utilcode.util.Utils;
+import com.joanzapata.iconify.IconFontDescriptor;
+import com.joanzapata.iconify.Iconify;
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.Logger;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
+
+import okhttp3.Interceptor;
+
 /**
  * 项目名   : xct-WanAndroid
  * 包名     : com.xct.core.app
@@ -8,22 +22,73 @@ package com.xct.core.app;
  * 创建时间 :  2020/5/16 18:12
  * 描述     :  全局配置
  */
-public class Configurator {
+public final class Configurator {
+
+    private static final HashMap<Object, Object> LATTE_CONFIGS = new HashMap<>();
+    private static final Handler HANDLER = new Handler();
+    private static final ArrayList<IconFontDescriptor> ICONS = new ArrayList<>();
+    private static final ArrayList<Interceptor> INTERCEPTORS = new ArrayList<>();
 
 
-//    private Configurator{
-//
-//
-//    }
+    private Configurator() {
+        LATTE_CONFIGS.put(ConfigKeys.CONFIG_READY, false);
+        LATTE_CONFIGS.put(ConfigKeys.HANDLER, HANDLER);
+    }
 
 
     public static Configurator getInstance() {
         return Holder.INSTANCE;
     }
 
-
     private static class Holder {
         private static Configurator INSTANCE = new Configurator();
+    }
+
+    final HashMap<Object, Object> getLatteConfigs() {
+        return LATTE_CONFIGS;
+    }
+
+    public final void configure(){
+        initIcons();
+        Logger.addLogAdapter(new AndroidLogAdapter());
+        LATTE_CONFIGS.put(ConfigKeys.CONFIG_READY,true);
+        Utils.init(Latte.getApplication());
+    }
+
+    private void initIcons(){
+        if(ICONS.size()>0){
+            final Iconify.IconifyInitializer initializer=Iconify.with(ICONS.get(0));
+            for (int i = 1; i <ICONS.size() ; i++) {
+                initializer.with(ICONS.get(i));
+
+            }
+        }
+    }
+
+    private Configurator withApiHost(){
+        return this;
+    }
+
+
+
+
+
+
+    private void checkConfiguration() {
+        final boolean isReady = (boolean) LATTE_CONFIGS.get(ConfigKeys.CONFIG_READY);
+        if (!isReady) {
+            throw new RuntimeException("Configuration is not ready,call configure");
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    final <T> T getConfiguration(Object key) {
+        checkConfiguration();
+        final Object value = LATTE_CONFIGS.get(key);
+        if (value == null) {
+            throw new NullPointerException(key.toString() + "IS NULL");
+        }
+        return (T) LATTE_CONFIGS.get(key);
     }
 
 }
--- xct_core/src/main/java/com/xct/core/app/Latte.java
+++ xct_core/src/main/java/com/xct/core/app/Latte.java
@@ -1,24 +1,42 @@
 package com.xct.core.app;
 
+import android.content.Context;
+import android.os.Handler;
+
 /**
  * 项目名   : xct-WanAndroid
  * 包名     : com.xct.core.app
  * 文件名   : Latte
  * 创建者   :  xct
  * 创建时间 :  2020/5/16 18:11
- * 描述     :  全局配置变量管理类
+ * 描述     :  全局配置变量管理类(对外)
  */
 public class Latte {
 
 
-
-    public static Configurator init(){
-//        Configurator.getInstance()
+    public static Configurator init(Context context) {
+        Configurator.getInstance()
+                .getLatteConfigs()
+                .put(ConfigKeys.APPLICATION_CONTEXT, context.getApplicationContext());
+        return Configurator.getInstance();
 
     }
 
-    public static Configurator getConfigurator(){
+    public static Configurator getConfigurator() {
         return Configurator.getInstance();
     }
+
+    public static <T> T getConfiguration(Object key) {
+        return getConfigurator().getConfiguration(key);
+    }
+
+    public static Context getApplication() {
+        return getConfiguration(ConfigKeys.APPLICATION_CONTEXT);
+    }
+
+    public static Handler getHandler() {
+        return getConfiguration(ConfigKeys.HANDLER);
+    }
+
 
 }
